4bcc9db0109049c417ed586a0df20541be1e50d3
jojoford <75343703+jojoford@users.noreply.github.com>
2021-03-28 00:05:52 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// Application library declaration
+const express = require('express')
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
